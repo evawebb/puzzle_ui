a48c38b2b87517ebd719a122e5e6751481526a35
Zack <ztwebb42@gmail.com>
2020-02-07 08:29:41 -0700

    organize fillomino in a much better way

--- fillomino.js
+++ fillomino.js
@@ -1,126 +1,127 @@
-var fillomino_grid_def = {
-  canvas_size: master_canvas_size,
-  grid_width: 10,
-  grid_height: 10,
-  edge_margin_multiplier: 0.2,
-  edge_width: 4
-};
+class Fillomino {
+  constructor() {
+    this.grid_def = {
+      canvas_size: master_canvas_size,
+      grid_width: 10,
+      grid_height: 10,
+      edge_margin_multiplier: 0.2,
+      edge_width: 4
+    };
+    this.cell_state = [];
+    this.edge_state = {};
+    this.selected = null;
 
-var fillomino_cell_state = [];
-var fillomino_edge_state = {};
-
-var fillomino_selected = null;
-
-function fillomino_setup() {
-  for (var y = 0; y < fillomino_grid_def.grid_height; y += 1) {
-    var cell_row = [];
-    for (var x = 0; x < fillomino_grid_def.grid_width; x += 1) {
-      cell_row.push("");
+    for (var y = 0; y < this.grid_def.grid_height; y += 1) {
+      var cell_row = [];
+      for (var x = 0; x < this.grid_def.grid_width; x += 1) {
+        cell_row.push("");
+      }
+      this.cell_state.push(cell_row);
     }
-    fillomino_cell_state.push(cell_row);
+
+    canvas.addEventListener("mouseup", this.on_mouseup.bind(this));
+    document.addEventListener("keydown", this.on_key.bind(this));
+    this.render();
   }
 
-  canvas.addEventListener("mouseup", fillomino_on_mouseup);
-  document.addEventListener("keydown", fillomino_on_key);
-  fillomino_render();
-}
+  on_mouseup(event) {
+    var x = event.pageX - canvas.offsetLeft;
+    var y = event.pageY - canvas.offsetTop;
+    console.log(this);
+    console.log(this.grid_def);
+    var cell_x = Math.floor((x - edge_margin(this.grid_def)) / min_cell_size(this.grid_def));
+    var cell_y = Math.floor((y - edge_margin(this.grid_def)) / min_cell_size(this.grid_def));
+    this.selected = [cell_x, cell_y];
 
-function fillomino_on_mouseup(event) {
-  var x = event.pageX - canvas.offsetLeft;
-  var y = event.pageY - canvas.offsetTop;
-  var cell_x = Math.floor((x - edge_margin(fillomino_grid_def)) / min_cell_size(fillomino_grid_def));
-  var cell_y = Math.floor((y - edge_margin(fillomino_grid_def)) / min_cell_size(fillomino_grid_def));
-  fillomino_selected = [cell_x, cell_y];
-
-  fillomino_render();
-}
-
-function fillomino_on_key(event) {
-  expand_grid(
-    event,
-    fillomino_grid_def,
-    [{
-      obj: fillomino_cell_state,
-      default: ""
-    }],
-    fillomino_render
-  );
-
-  if (["1", "2", "3", "4", "5", "6", "7", "8", "9", "Delete", "x", "t", "T"].includes(event.key) && fillomino_selected) {
-    if (event.key == "Delete" || event.key == "x") {
-      fillomino_cell_state[fillomino_selected[1]][fillomino_selected[0]] = "";
-    } else if (event.key == "t") {
-      if (fillomino_cell_state[fillomino_selected[1]][fillomino_selected[0]] == "") {
-        fillomino_cell_state[fillomino_selected[1]][fillomino_selected[0]] = "10";
-      } else {
-        fillomino_cell_state[fillomino_selected[1]][fillomino_selected[0]] = 10 + parseInt(fillomino_cell_state[fillomino_selected[1]][fillomino_selected[0]]);
-      }
-    } else if (event.key == "T") {
-      if (parseInt(fillomino_cell_state[fillomino_selected[1]][fillomino_selected[0]]) == 10) {
-        fillomino_cell_state[fillomino_selected[1]][fillomino_selected[0]] = "";
-      } else if (parseInt(fillomino_cell_state[fillomino_selected[1]][fillomino_selected[0]]) > 10) {
-        fillomino_cell_state[fillomino_selected[1]][fillomino_selected[0]] = parseInt(fillomino_cell_state[fillomino_selected[1]][fillomino_selected[0]]) - 10;
-      }
-    } else {
-      fillomino_cell_state[fillomino_selected[1]][fillomino_selected[0]] = event.key;
-    }
-  } else if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(event.key) && fillomino_selected) {
-    if (event.key == "ArrowUp" && fillomino_selected[1] > 0) {
-      fillomino_selected[1] -= 1;
-    } else if (event.key == "ArrowDown" && fillomino_selected[1] < fillomino_grid_def.grid_height - 1) {
-      fillomino_selected[1] += 1;
-    } else if (event.key == "ArrowLeft" && fillomino_selected[0] > 0) {
-      fillomino_selected[0] -= 1;
-    } else if (event.key == "ArrowRight" && fillomino_selected[0] < fillomino_grid_def.grid_width - 1) {
-      fillomino_selected[0] += 1;
-    }
-  } else if (["w", "a", "s", "d"].includes(event.key) && fillomino_selected) {
-    if (event.key == "w" && fillomino_selected[1] > 0) {
-      toggle_edge_state(fillomino_edge_state, fillomino_selected[0], fillomino_selected[1], 1);
-    } else if (event.key == "a" && fillomino_selected[0] > 0) {
-      toggle_edge_state(fillomino_edge_state, fillomino_selected[0], fillomino_selected[1], 2);
-    } else if (event.key == "s" && fillomino_selected[1] < fillomino_grid_def.grid_height - 1) {
-      toggle_edge_state(fillomino_edge_state, fillomino_selected[0], fillomino_selected[1] + 1, 1);
-    } else if (event.key == "d" && fillomino_selected[0] < fillomino_grid_def.grid_width - 1) {
-      toggle_edge_state(fillomino_edge_state, fillomino_selected[0] + 1, fillomino_selected[1], 2);
-    }
+    this.render();
   }
 
-  fillomino_render();
-}
-
-function fillomino_render() {
-  context.clearRect(0, 0, fillomino_grid_def.canvas_size, fillomino_grid_def.canvas_size);
-  fillomino_draw_fillomino_selected();
-  draw_grid(fillomino_grid_def, fillomino_edge_state);
-  fillomino_draw_numbers();
-}
-
-function fillomino_draw_fillomino_selected() {
-  if (fillomino_selected) {
-    context.fillStyle = "rgba(0, 255, 0, 0.5)";
-    context.fillRect(
-      fillomino_selected[0] * min_cell_size(fillomino_grid_def) + edge_margin(fillomino_grid_def),
-      fillomino_selected[1] * min_cell_size(fillomino_grid_def) + edge_margin(fillomino_grid_def),
-      min_cell_size(fillomino_grid_def),
-      min_cell_size(fillomino_grid_def)
+  on_key(event) {
+    expand_grid(
+      event,
+      this.grid_def,
+      [{
+        obj: this.cell_state,
+        default: ""
+      }],
+      this.render
     );
-  }
-}
 
-function fillomino_draw_numbers() {
-  context.font = "" + Math.floor(min_cell_size(fillomino_grid_def) * 0.8) + "px serif";
-  context.fillStyle = "#000000";
-  context.textAlign = "center";
-  context.textBaseline = "middle";
-  for (var y = 0; y < fillomino_grid_def.grid_height; y += 1) {
-    for (var x = 0; x < fillomino_grid_def.grid_width; x += 1) {
-      context.fillText(
-        fillomino_cell_state[y][x],
-        ((x + 0.5) * min_cell_size(fillomino_grid_def)) + edge_margin(fillomino_grid_def),
-        ((y + 0.5) * min_cell_size(fillomino_grid_def)) + edge_margin(fillomino_grid_def)
+    if (["1", "2", "3", "4", "5", "6", "7", "8", "9", "Delete", "x", "t", "T"].includes(event.key) && this.selected) {
+      if (event.key == "Delete" || event.key == "x") {
+        this.cell_state[this.selected[1]][this.selected[0]] = "";
+      } else if (event.key == "t") {
+        if (this.cell_state[this.selected[1]][this.selected[0]] == "") {
+          this.cell_state[this.selected[1]][this.selected[0]] = "10";
+        } else {
+          this.cell_state[this.selected[1]][this.selected[0]] = 10 + parseInt(this.cell_state[this.selected[1]][this.selected[0]]);
+        }
+      } else if (event.key == "T") {
+        if (parseInt(this.cell_state[this.selected[1]][this.selected[0]]) == 10) {
+          this.cell_state[this.selected[1]][this.selected[0]] = "";
+        } else if (parseInt(this.cell_state[this.selected[1]][this.selected[0]]) > 10) {
+          this.cell_state[this.selected[1]][this.selected[0]] = parseInt(this.cell_state[this.selected[1]][this.selected[0]]) - 10;
+        }
+      } else {
+        this.cell_state[this.selected[1]][this.selected[0]] = event.key;
+      }
+    } else if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(event.key) && this.selected) {
+      if (event.key == "ArrowUp" && this.selected[1] > 0) {
+        this.selected[1] -= 1;
+      } else if (event.key == "ArrowDown" && this.selected[1] < this.grid_def.grid_height - 1) {
+        this.selected[1] += 1;
+      } else if (event.key == "ArrowLeft" && this.selected[0] > 0) {
+        this.selected[0] -= 1;
+      } else if (event.key == "ArrowRight" && this.selected[0] < this.grid_def.grid_width - 1) {
+        this.selected[0] += 1;
+      }
+    } else if (["w", "a", "s", "d"].includes(event.key) && this.selected) {
+      if (event.key == "w" && this.selected[1] > 0) {
+        toggle_edge_state(this.edge_state, this.selected[0], this.selected[1], 1);
+      } else if (event.key == "a" && this.selected[0] > 0) {
+        toggle_edge_state(this.edge_state, this.selected[0], this.selected[1], 2);
+      } else if (event.key == "s" && this.selected[1] < this.grid_def.grid_height - 1) {
+        toggle_edge_state(this.edge_state, this.selected[0], this.selected[1] + 1, 1);
+      } else if (event.key == "d" && this.selected[0] < this.grid_def.grid_width - 1) {
+        toggle_edge_state(this.edge_state, this.selected[0] + 1, this.selected[1], 2);
+      }
+    }
+
+    this.render();
+  }
+
+  render() {
+    context.clearRect(0, 0, this.grid_def.canvas_size, this.grid_def.canvas_size);
+    this.draw_selected();
+    draw_grid(this.grid_def, this.edge_state);
+    this.draw_numbers();
+  }
+
+  draw_selected() {
+    if (this.selected) {
+      context.fillStyle = "rgba(0, 255, 0, 0.5)";
+      context.fillRect(
+        this.selected[0] * min_cell_size(this.grid_def) + edge_margin(this.grid_def),
+        this.selected[1] * min_cell_size(this.grid_def) + edge_margin(this.grid_def),
+        min_cell_size(this.grid_def),
+        min_cell_size(this.grid_def)
       );
     }
   }
-}
 
+  draw_numbers() {
+    context.font = "" + Math.floor(min_cell_size(this.grid_def) * 0.8) + "px serif";
+    context.fillStyle = "#000000";
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    for (var y = 0; y < this.grid_def.grid_height; y += 1) {
+      for (var x = 0; x < this.grid_def.grid_width; x += 1) {
+        context.fillText(
+          this.cell_state[y][x],
+          ((x + 0.5) * min_cell_size(this.grid_def)) + edge_margin(this.grid_def),
+          ((y + 0.5) * min_cell_size(this.grid_def)) + edge_margin(this.grid_def)
+        );
+      }
+    }
+  }
+}
